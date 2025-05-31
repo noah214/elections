f289a7d2ccc8bd3857e4ce5c9cdf0a430018e22e
noah214 <leonardpanergo@gmail.com>
2025-05-31 08:51:06 +0800

    final change

--- app/public/home.php
+++ app/public/home.php
@@ -43,7 +43,7 @@ session_start();
                     <div class="vr mx-2 d-none d-lg-block"></div>
                     <a class="nav-item nav-link" href="vote.php">Vote</a>
                     <div class="vr mx-2 d-none d-lg-block"></div>
-                    <a class="nav-item nav-link" href="Account.php">Account</a>
+                    <a class="nav-item nav-link" href="account.php">Account</a>
                 </div>
             </div>
         </div>
